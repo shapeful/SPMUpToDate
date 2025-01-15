3b6497ebe7cf5b2788360f72cd1d58e79898922e
Nick Trienens <nicktrienens@gmail.com>
2025-01-14 22:52:46 -0700

    clean up

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/skelpo/json.git", branch: "main"),
-//        .package(url: "https://github.com/freshOS/Networking.git", from: "2.0.3"),
+        //        .package(url: "https://github.com/freshOS/Networking.git", from: "2.0.3"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.2"),
-//        .package(url: "https://github.com/shapeful/swift-package-list.git", branch: "master")
+        //        .package(url: "https://github.com/shapeful/swift-package-list.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -28,10 +28,10 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "JSON", package: "json"),
-//                .product(name: "Networking", package: "networking"),
+                //                .product(name: "Networking", package: "networking"),
                 .product(name: "Alamofire", package: "alamofire"),
-//                .product(name: "SwiftPackageList", package: "swift-package-list"),
+                //                .product(name: "SwiftPackageList", package: "swift-package-list"),
             ]
-        ),
+        )
     ]
 )
