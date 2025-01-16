c575188c987595841bfb06b652d1ec40486f5cb2
Nick Trienens <nicktrienens@gmail.com>
2025-01-16 07:11:18 -0700

    SPM additions

--- Package.swift
+++ Package.swift
@@ -8,12 +8,13 @@ let package = Package(
     platforms: [
         .macOS(.v10_15),
     ],
+    products: [
+         .executable(name: "SPMUpToDate", targets: ["SPMUpToDate"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/skelpo/json.git", branch: "main"),
-        //        .package(url: "https://github.com/freshOS/Networking.git", from: "2.0.3"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.2"),
-        //        .package(url: "https://github.com/shapeful/swift-package-list.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -23,9 +24,8 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "JSON", package: "json"),
-                //                .product(name: "Networking", package: "networking"),
                 .product(name: "Alamofire", package: "alamofire"),
-                //                .product(name: "SwiftPackageList", package: "swift-package-list"),
+                // .product(name: "SwiftPackageList", package: "swift-package-list"),
             ]
         )
     ]
